4a1a7b2ce978c1511ae9da24f239cebf0b685922
MrWaffle <mrwafflewaffle@aim.com>
2013-11-27 20:37:39 +0100

    Include console test

---
+++ Communicator/Communicator.Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Communicator;
+
+namespace Communicator.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RESTCommunicator db = new RESTCommunicator("http://127.0.0.1:5000");
+            //db.GetFromREST("profiles", 1, 1);
+            Processor pc = new Processor("http://127.0.0.1:5000");
+            while (true)
+            {
+                Console.Clear();
+                PrintAll(pc);
+                Console.ReadKey();
+            }
+
+        }
+
+        static void PrintAll(Processor pc)
+        {
+            foreach (Profile w in pc.GetNextProfiles())
+            {
+                Console.WriteLine(w.name.firstname);
+            }
+        }
+    }
+}
